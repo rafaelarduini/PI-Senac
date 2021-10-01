b42d573936534b0ed7f806f189803843b07676fd
Rafael Arduini <rafaelarduini@hotmail.com>
2021-10-01 10:49:32 -0300

    Create MenuFornecedor.java
    Inclusão menu fornecedor.

---
+++ PI/src/MenuFornecedor.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class MenuFornecedor {
+	
+public void AbrirMenu() {
+		
+	Scanner menu = new Scanner (System.in);
+	
+	try {	
+	
+		while (true) {
+			System.out.print("\nMenu Fornecedor\n\n");
+	        System.out.print("Opção 1 - Cadastrar \n");
+	        System.out.print("Opção 2 - Atualizar \n");
+	        System.out.print("Opção 3 - Excluir \n");
+	        System.out.print("Opção 4 - Retornar \n\n");
+	        System.out.print("Digite uma opção: ");
+	
+	        String opcao = menu.nextLine();
+	
+	        switch (opcao) {
+	        case "1":	        	
+	        	//Cadastrar();		        	
+	            break;
+	
+	        case "2":
+	        	//Atualizar();
+	            break;
+	
+	        case "3":
+	        	//Excluir();
+	            break;
+	            
+	        case "4":
+	        	return;
+	
+	        default:
+	            System.out.print("\nOpção Inválida!\n");
+	            break;		            
+	        }
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+			menu.close();
+		}			
+	}	
+}
